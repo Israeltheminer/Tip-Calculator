074cb163630ae030f009cf311c521949eeef227a
Israeltheminer <israeliyanda5@gmail.com>
2022-05-02 08:35:33 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flyo-Dark-Landing-Page
+Code Challenge by Code Mentor coded by Iyanda Israel
+
+## Resourses Used
+### VsCode
+### jQuery
+### Postcss
---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+   plugins: [
+      require('postcss-import-ext-glob'),
+      require('postcss-import'),
+      require('cssnano'),
+      require('postcss-preset-env')({stage: 1}),
+      require('tailwindcss/nesting'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+      process.env.NODE_ENV === "production" && require('@fullhuman/postcss-purgecss')({
+         content:[
+            './src/**/*.{html,js}',
+            'index.html'
+         ],
+         defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      }),
+   ]
+ }
